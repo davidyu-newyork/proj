b85f06c0a8f1402ac6dfa0549bcbb8d7522f1ca8
David Yu <53448440+davidyu-newyork@users.noreply.github.com>
2021-01-10 14:55:34 -0500

    added routes and components

---
+++ react-app/src/components/Main-Page.component.js
@@ -0,0 +1,15 @@
+//import { render } from '@testing-library/react';
+import React, {Component } from 'react';
+//import {Link} from 'react-router-dom';
+
+
+export default class MainPage extends Component{
+
+    render(){
+        return(
+            <h1>hello</h1>
+
+
+        )
+    }
+}
